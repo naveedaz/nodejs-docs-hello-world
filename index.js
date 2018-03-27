e27651fffb5b041957514691d43187b0a116019a
Naveed Aziz <naveeda@microsoft.com>
2018-03-28 00:45:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+    response.end("<!DOCTYPE html><html><head><style>p {color: red;}</style></head><body>Hello World</body></html>");
 });
 
 var port = process.env.PORT || 1337;
